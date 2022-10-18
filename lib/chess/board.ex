2d95382d094ccb86b3c5b4b72e0c534fe665e362
Javier Quirós <javiquiros@hotmail.com>
2022-10-19 00:03:52 +0200

    Move board logic

--- lib/chess/board.ex
+++ lib/chess/board.ex
@@ -8,4 +8,52 @@ defmodule Chess.Board do
 
   @spec get_y_dimension :: pos_integer
   def get_y_dimension(), do: @y_dimension
+
+
+  def get_square_color(x, y, selected_square) do
+    # TODO: Fix this shit
+    if selected_square == x_y_into_chess_square(x, y) do
+      "selected"
+    else
+      if rem(y, 2) == 0 do
+        if rem(x, 2) == 0 do
+          "black"
+        else
+          "white"
+        end
+      else
+        if rem(x, 2) == 0 do
+          "white"
+        else
+          "black"
+        end
+      end
+    end
+  end
+
+  def is_piece(x, y, pieces) do
+    letter = x_y_into_chess_square(x, y)
+    piece = Map.get(pieces, letter)
+    piece_into_class(piece)
+  end
+
+  def x_y_into_chess_square(x, y) do
+    letter = List.to_string([64 + y])
+    "#{letter}#{x}"
+  end
+
+  def piece_into_class(nil), do: nil
+  def piece_into_class({is_black, type}) do
+    "#{black_into_class(is_black)} #{type_into_class(type)}"
+  end
+
+  def black_into_class(true), do: "black"
+  def black_into_class(false), do: "white"
+
+  def type_into_class("k"), do: "king"
+  def type_into_class("q"), do: "queen"
+  def type_into_class("r"), do: "rook"
+  def type_into_class("b"), do: "bishop"
+  def type_into_class("kn"), do: "knight"
+  def type_into_class("p"), do: "pawn"
 end
--- lib/chess_web/game_live.ex
+++ lib/chess_web/game_live.ex
@@ -1,6 +1,8 @@
 defmodule ChessWeb.GameLive do
   use ChessWeb, :live_view
 
+  import Chess.Board
+
   alias ChessWeb.BoardLive
   alias ChessWeb.GameServer
   alias ChessWeb.BoardLive.Components
@@ -36,27 +38,17 @@ defmodule ChessWeb.GameLive do
     }
   end
 
-  def handle_info(%{event: "piece_moved", payload: %{"square" => square}}, socket) do
-    old_square = socket.assigns.selected_square
-    if old_square do
-      pieces = Map.delete(socket.assigns.pieces, square)
-      GameServer.move_piece(socket.assigns.pid, %{"old_square" => old_square, "new_square" => square})
-      {:noreply,
-        socket
-        |> assign(:pieces, pieces)
-        |> assign(:selected_square, nil)}
-    else
-      {:noreply,
+  def handle_info(%{event: "piece_moved"}, socket) do
+    {:noreply,
       socket
-      |> assign(:selected_square, square)}
-    end
+      |> assign_pieces()
+      |> assign(:selected_square, nil)}
   end
 
   defp assign_pieces(socket) do
     pieces = GameServer.get_pieces(socket.assigns.pid)
     assign(socket, :pieces, pieces)
   end
-
 
   # Board
 
@@ -88,54 +80,6 @@ defmodule ChessWeb.GameLive do
     """
   end
 
-  defp get_square_color(x, y, selected_square) do
-    # TODO: Fix this shit
-    if selected_square == x_y_into_chess_square(x, y) do
-      "selected"
-    else
-      if rem(y, 2) == 0 do
-        if rem(x, 2) == 0 do
-          "black"
-        else
-          "white"
-        end
-      else
-        if rem(x, 2) == 0 do
-          "white"
-        else
-          "black"
-        end
-      end
-    end
-  end
-
-  defp is_piece(x, y, pieces) do
-    letter = x_y_into_chess_square(x, y)
-    piece = Map.get(pieces, letter)
-    piece_into_class(piece)
-  end
-
-  defp x_y_into_chess_square(x, y) do
-    letter = List.to_string([64 + y])
-    "#{letter}#{x}"
-  end
-
-  defp piece_into_class(nil), do: nil
-  defp piece_into_class({is_black, type}) do
-    "#{black_into_class(is_black)} #{type_into_class(type)}"
-  end
-
-  defp black_into_class(true), do: "black"
-  defp black_into_class(false), do: "white"
-
-  defp type_into_class("k"), do: "king"
-  defp type_into_class("q"), do: "queen"
-  defp type_into_class("r"), do: "rook"
-  defp type_into_class("b"), do: "bishop"
-  defp type_into_class("kn"), do: "knight"
-  defp type_into_class("p"), do: "pawn"
-
-
   def update(assigns, socket) do
     {:ok,
     socket
@@ -150,7 +94,6 @@ defmodule ChessWeb.GameLive do
       {y, _} = Integer.parse(y)
       square = x_y_into_chess_square(x, y)
       selected_square = socket.assigns.selected_square
-      IO.inspect(square)
       if square == selected_square do
         selected_square == nil
       end
@@ -158,29 +101,31 @@ defmodule ChessWeb.GameLive do
     end
 
     @impl Phoenix.LiveView
-    def handle_event("move_piece_to", %{"x" => x, "y" => y}, socket) do
-      selected_square = socket.assigns.selected_square
-      if selected_square do
-        {x, _} = Integer.parse(x)
-        {y, _} = Integer.parse(y)
-        square = x_y_into_chess_square(x, y)
-        {:noreply, handle_piece_moved(socket, "bk", square)}
-      else
-        {:noreply, socket}
-      end
+    def handle_event("move_piece_to", _, %{assigns: %{selected_square: nil}} = socket), do: {:noreply, socket}
+
+    def handle_event(
+      "move_piece_to",
+      %{"x" => x, "y" => y},
+      %{assigns: %{selected_square: selected_square}} = socket) do
+      {x, _} = Integer.parse(x)
+      {y, _} = Integer.parse(y)
+      square = x_y_into_chess_square(x, y)
+      GameServer.move_piece(socket.assigns.pid, %{"old_square" => selected_square, "new_square" => square})
+      {:noreply, handle_piece_moved(socket, "bk", square)}
     end
 
     defp handle_piece_moved(
-      %{assigns: %{pieces: pieces}} = socket,
+      %{assigns: %{pieces: pieces, selected_square: selected_square}} = socket,
       piece,
       square
     ) do
-      ChessWeb.Endpoint.broadcast(socket.assigns.chess_game_topic_id, "piece_moved", %{"square" => square})
+      ChessWeb.Endpoint.broadcast(socket.assigns.chess_game_topic_id, "piece_moved", %{})
       socket
       |> put_flash(:info, "Rating submitted successfully")
       |> assign(
       :pieces,
-      Map.delete(socket.assigns.pieces, socket.assigns.selected_square)
+      Map.delete(socket.assigns.pieces, selected_square)
+      |> assign(:selected_square, nil)
       )
     end
 end
