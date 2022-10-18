50e9f210a5fd15ae2d0ba9d4f5b082993100dc22
Javier Quirós <javiquiros@hotmail.com>
2022-10-18 18:23:24 +0200

    Move initial position to view

--- lib/chess/pieces.ex
+++ lib/chess/pieces.ex
@@ -5,8 +5,42 @@ defmodule Chess.Pieces do
   @knight "knight"
   @bishop "bishop"
   @pawn "pawn"
+
+  @initial_position %{
+    "A1" => {false, "r"},
+    "A2" => {false, "kn"},
+    "A3" => {false, "b"},
+    "A4" => {false, "q"},
+    "A5" => {false, "k"},
+    "A6" => {false, "b"},
+    "A7" => {false, "kn"},
+    "A8" => {false, "r"},
+    "B1" => {false, "p"},
+    "B2" => {false, "p"},
+    "B3" => {false, "p"},
+    "B4" => {false, "p"},
+    "B5" => {false, "p"},
+    "B6" => {false, "p"},
+    "B7" => {false, "p"},
+    "B8" => {false, "p"},
+    "H1" => {true, "r"},
+    "H2" => {true, "kn"},
+    "H3" => {true, "b"},
+    "H4" => {true, "q"},
+    "H5" => {true, "k"},
+    "H6" => {true, "b"},
+    "H7" => {true, "kn"},
+    "H8" => {true, "r"},
+    "G1" => {true, "p"},
+    "G2" => {true, "p"},
+    "G3" => {true, "p"},
+    "G4" => {true, "p"},
+    "G5" => {true, "p"},
+    "G6" => {true, "p"},
+    "G7" => {true, "p"},
+    "G8" => {true, "p"},
+  }
+
+  def get_initial_position, do: @initial_position
+
 end
-
-
-# <div class="figure white rook"></div>
-# <div class="figure black rook"></div>
