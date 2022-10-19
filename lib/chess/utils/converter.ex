8e469f59589d5ec27825eb10875fca6d89b9befb
Maksym Kosenko <maxie7@protonmail.com>
2022-10-19 09:40:00 +0200

    Fix css... etc

--- lib/chess/utils/converter.ex
+++ lib/chess/utils/converter.ex
@@ -1,18 +1,43 @@
 defmodule Chess.Utils.Converter do
   @minute 60
   @hour @minute * 60
-  @day @hour * 24
-  @week @day * 7
-  @divisor [@week, @day, @hour, @minute, 1]
 
-  def sec_to_str(sec) do
-    {_, [s, m, h, d, w]} =
-      Enum.reduce(@divisor, {sec, []}, fn divisor, {n, acc} ->
-        {rem(n, divisor), [div(n, divisor) | acc]}
-      end)
+  def sec_to_str(seconds) when seconds >= @hour do
+    h = div(seconds, @hour)
 
-    ["#{w} wk", "#{d} d", "#{h} hr", "#{m} min", "#{s} sec"]
-    |> Enum.reject(fn str -> String.starts_with?(str, "0") end)
-    |> Enum.join(", ")
+    m =
+      seconds
+      |> rem(@hour)
+      |> div(@minute)
+      |> pad_int()
+
+    s =
+      seconds
+      |> rem(@hour)
+      |> rem(@minute)
+      |> pad_int()
+
+    "#{h}:#{m}:#{s}"
+  end
+
+  def sec_to_str(seconds) do
+    m = div(seconds, @minute)
+
+    s =
+      seconds
+      |> rem(@minute)
+      |> pad_int()
+
+    "#{m}:#{s}"
+  end
+
+  ###########
+  # PRIVATE #
+  ###########
+
+  defp pad_int(int, padding \\ 2) do
+    int
+    |> Integer.to_string()
+    |> String.pad_leading(padding, "0")
   end
 end
